723e18175122987d2b750a1c82502e9a77b1c66f
atafatta <atafatta@gmail.com>
2022-10-30 19:01:05 +0700

    menambahkan controller java

---
+++ src/main/java/tugastakehome/tugastakehomesatu/datacontroller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugastakehome.tugastakehomesatu;
+
+/**
+ *
+ * @author STRIX
+ */
+public class datacontroller {
+    
+}
