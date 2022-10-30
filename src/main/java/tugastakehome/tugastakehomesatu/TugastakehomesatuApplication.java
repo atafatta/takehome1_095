4cb5a126407d7edb9bfe46fd90f4b8e5318879b2
atafatta <atafatta@gmail.com>
2022-10-30 18:47:36 +0700

    membuat file baru

---
+++ src/main/java/tugastakehome/tugastakehomesatu/TugastakehomesatuApplication.java
@@ -0,0 +1,13 @@
+package tugastakehome.tugastakehomesatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugastakehomesatuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugastakehomesatuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tugastakehome/tugastakehomesatu/TugastakehomesatuApplicationTests.java
@@ -0,0 +1,13 @@
+package tugastakehome.tugastakehomesatu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugastakehomesatuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
